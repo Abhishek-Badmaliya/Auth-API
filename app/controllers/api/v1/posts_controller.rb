39866d8107f4cb83c8af84946d7cf9059f9e98ff
Abhishek-Badmaliya <abhishek.badmaliya@simformsolutions.com>
2023-01-31 13:25:26 +0530

    feat: implement post crud and api controller

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class Api::V1::PostsController < ApiController
+  before_action :set_post, only: %i[show update destroy]
+
+  def index
+    # @posts = Post.all
+    @posts = current_user.posts
+    render json: @posts, status: :ok
+  end
+
+  def show
+    render json: @post, status: :ok
+  end
+
+  def create
+    # @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      render json: @post, status: :ok
+    else
+      render json: { data: @post.errors.full_messages, status: 'failed' }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post, status: :ok
+    else
+      render json: { data: @post.errors.full_messages, status: 'failed' }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      render json: { data: 'Your post has been deleted successfully !', status: 'success' }, status: :ok
+    else
+      render json: { data: 'Oops, something went wrong !', status: 'failed' }
+    end
+  end
+
+  private
+
+  def set_post
+    # @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: e.message, status: :unauthorized
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   }
 
   get '/member_details' => 'members#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
