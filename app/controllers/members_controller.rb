dcfaca287488abd5aa52cdfed3ca3d6f818c01de
Abhishek-Badmaliya <abhishek.badmaliya@simformsolutions.com>
2023-01-31 11:45:12 +0530

    feat: access signed in user in app

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class MembersController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    render json: current_user, status: :ok
+  end
+end
