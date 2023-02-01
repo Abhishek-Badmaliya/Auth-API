f347f475a218b94afa4f19c6dee01a3d045406ca
Abhishek-Badmaliya <abhishek.badmaliya@simformsolutions.com>
2023-02-01 13:21:25 +0530

    feat : add exception error handling

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class ApiController < ApplicationController
-  before_action :authenticate_user!
-
-  rescue_from CanCan::AccessDenied do |exception|
-    render json: { warning: exception, status: 'Authorization failed !' }
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError do |e|
+    if e.message.include?('Error occurred while parsing request parameters')
+      render json: { error: 'Please provide required information properly to create a company' }, status: 400
+    elsif e.message.include?("Couldn't find")
+      render json: { error: "No record exists with given ID = #{params[:id]}" }, status: 404
+    else
+      e.message.include?('You are not authorized to access this page.')
+      render json: { error: 'You are not authorized to access this page.' }, status: 401
+    end
+  end
 end
